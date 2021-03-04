c6ea5464e56d212ab079958d6fe20ba3426b5828
chenyahui <cyhone@qq.com>
2021-03-04 11:25:16 +0800

    fix pointer typo

--- pointer/pointer.go
+++ pointer/pointer.go
@@ -12,6 +12,14 @@ func Int(x int) *int {
 	return &x
 }
 
-func Int64(x int) *int {
+func Int32(x int32) *int32 {
+	return &x
+}
+
+func Int64(x int64) *int64 {
+	return &x
+}
+
+func String(x string) *string {
 	return &x
 }
