936212e90b118e5a97425d7db3b93de242f1231c
chenyahui <cyhone@qq.com>
2021-03-17 16:33:57 +0800

    feat: add slice.DumpStrings

--- slice/dump.go
+++ slice/dump.go
@@ -1,10 +1,7 @@
 package slice
 
-func DumpStrings(strSlice []string) []string {
-	result := []string{}
-	for _, item := range strSlice {
-		result = append(result, item)
-	}
-
-	return result
+func DumpStrings(src []string) []string {
+	dst := make([]string, len(src))
+	copy(dst, src)
+	return dst
 }
