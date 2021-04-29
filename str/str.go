94096d0448ebed694840b74cb708f3b679f97fea
chenyahui <cyhone@qq.com>
2021-04-29 20:57:10 +0800

    feat: add str.IsNumber

--- str/str.go
+++ str/str.go
@@ -1,6 +1,9 @@
 package str
 
-import "strings"
+import (
+	"strconv"
+	"strings"
+)
 
 func ReplaceSubString(old string, from int, to int, new string) string {
 	return old[0:from] + new + old[to:]
@@ -9,4 +12,10 @@ func ReplaceSubString(old string, from int, to int, new string) string {
 // IsBlank check a string is whitespace
 func IsBlank(val string) bool {
 	return len(strings.TrimSpace(val)) == 0
+}
+
+// IsNumber check a string is number
+func IsNumber(val string) bool {
+	_, err := strconv.ParseInt(val, 10, 64)
+	return err == nil
 }
